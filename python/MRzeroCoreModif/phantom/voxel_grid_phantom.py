ca46b52d870ee45927fb10bd2aa266f28b8f7a1b
AureleM1958 <aurele.monfort@gmail.com>
2025-01-31 18:25:00 +0100

    coucou

--- python/MRzeroCoreModif/phantom/voxel_grid_phantom.py
+++ python/MRzeroCoreModif/phantom/voxel_grid_phantom.py
@@ -191,7 +191,7 @@ class VoxelGridPhantom:
 
     @classmethod
     def load(cls, file_name: str) -> VoxelGridPhantom:
-        """Load a phantom from data produced by `generate_maps.py`."""
+        """Load a phantom from data produced by `generate_maps.py`. cecci est une modif"""
         with np.load(file_name,allow_pickle=True) as data:
             T1 = torch.tensor(data['T1_map'])
             T2 = torch.tensor(data['T2_map'])
